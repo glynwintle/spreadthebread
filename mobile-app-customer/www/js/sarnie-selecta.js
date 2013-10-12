dacf7b5df1b9877b767a1949c47a37b2968d6f96
Lewis Westbury <lewiswestbury@lewiswestbury-macbookair2.roam.corp.google.com>
2013-10-12 14:13:53 +0100

    Initial commit.

---
+++ mobile-app-customer/www/js/sarnie-selecta.js
@@ -0,0 +1,37 @@
+
+/*** page transitions ***/
+
+function visit_something() { jQuery.mobile.changePage('something.html', { transition: 'slide' }); }
+
+/*** page load events ***/
+
+// sample from detox
+// $(document).delegate('#posts-list', 'pageshow', load_post_list);
+// function load_post_list() { remote_retrieve_posts(); }
+
+/*** team quotes ***/
+
+// you'd make a great hobbit
+// can we just make a GUI hand job joke now and get it out of the way
+
+/*** potential project titles ***/
+
+// spread the bread
+// bread spread
+// sandwich girl
+// sarnie selecta
+// hand job
+// you'll get it when you get it
+// cloud sandwich
+// sandwich as a service
+
+/*** clean quotes ***/
+
+// we'll have to de-dupe the sandwiches
+
+
+/*** inspirational project names ***/
+
+// community chest
+// village bicycle
+
---
+++ mobile-app-seller/platforms/ios/www/js/sarnie-selecta.js
@@ -0,0 +1,20 @@
+
+/*** page transitions ***/
+
+function visit_something() { jQuery.mobile.changePage('something.html', { transition: 'slide' }); }
+
+/*** page load events ***/
+
+// sample from detox
+// $(document).delegate('#posts-list', 'pageshow', load_post_list);
+// function load_post_list() { remote_retrieve_posts(); }
+
+/*** team notes ***/
+
+// you'd make a great hobbit
+// can we just make a GUI hand job joke now and get it out of the way
+
+// inspirational project names:
+// community chest
+// village bicycle
+
